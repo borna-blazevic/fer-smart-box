4b7453b3a67a40572e80108f559834e7d897b49d
borna-blazevic <borna.blazevic@fer.hr>
2019-12-06 20:03:14 +0100

    add wifi connection

---
+++ components/wifi/wifi.c
@@ -0,0 +1,127 @@
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+
+#include "wifi.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+/* The examples use WiFi configuration that you can set via project configuration menu
+
+   If you'd rather not, just change the below entries to strings with
+   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+*/
+#define WIFI_SSID CONFIG_WIFI_SSID
+#define WIFI_PASSWORD CONFIG_WIFI_PASSWORD
+#define MAXIMUM_RETRY CONFIG_MAXIMUM_RETRY
+
+#if defined(CONFIG_LIMIT_NUMBER_OF_RETRIES)
+
+#define LIMIT_NUMBER_OF_RETRIES CONFIG_LIMIT_NUMBER_OF_RETRIES
+
+#endif // LIMIT_NUMBER_OF_RETRIES
+
+#if !defined(CONFIG_LIMIT_NUMBER_OF_RETRIES)
+
+#define LIMIT_NUMBER_OF_RETRIES false
+
+#endif // LIMIT_NUMBER_OF_RETRIES
+
+
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+const int WIFI_CONNECTED_BIT = BIT0;
+int connected = 0;
+
+/* The event group allows multiple bits for each event, but we only care about one event 
+ * - are we connected to the AP with an IP? */
+
+static const char *TAG = "wifi station";
+
+static int s_retry_num = 0;
+
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (LIMIT_NUMBER_OF_RETRIES)
+        {
+            if (s_retry_num < MAXIMUM_RETRY)
+            {
+                esp_wifi_connect();
+                xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+                s_retry_num++;
+                connected = 0;
+                ESP_LOGI(TAG, "retry to connect to the AP");
+            }
+        }
+        else
+        {
+            ESP_LOGI(TAG, "retry to connect to the AP");
+            esp_wifi_connect();
+            xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+            connected = 0;
+        }
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+        connected = 1;
+    }
+}
+
+void wifi_init_sta()
+{
+    s_wifi_event_group = xEventGroupCreate();
+    esp_netif_init();
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASSWORD},
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+    ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
+             WIFI_SSID, WIFI_PASSWORD);
+}
+
+esp_err_t wifi_connect(void)
+{
+    wifi_init_sta();
+    ESP_LOGI(TAG, "Waiting for IP");
+    xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT, true, true, portMAX_DELAY);
+    return ESP_OK;
+}
+
+int wifi_is_connected(void)
+{
+    return connected;
+}
---
+++ components/wifi/wifi.h
@@ -0,0 +1,4 @@
+
+void wifi_init_sta(void);
+int wifi_is_connected(void);
+esp_err_t wifi_connect(void);
